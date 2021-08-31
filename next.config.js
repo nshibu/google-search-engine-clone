791510d979298de1c66514ea490f0caf80322fe1
Shibu N <shibu8870@gmail.com>
2021-08-31 08:12:49 +0530

    fix webpack issue

--- next.config.js
+++ next.config.js
@@ -3,4 +3,15 @@ module.exports = {
    images: {
     domains: ['assets.stickpng.com'],
   },
+   
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+            config.resolve.fallback = {
+                fs: false
+            }
+        }
+
+        return config;
+    }
 }
